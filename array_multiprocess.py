ac458aa1f8702e38911797e4a6271f9908ccbc5e
Aaron W <aaron.y.w@live.com.au>
2021-03-12 11:37:02 +1100

    multiprocessing optimized and case_101 result updated

--- array_multiprocess.py
+++ array_multiprocess.py
@@ -1,36 +1,44 @@
-import atsp, sys, multiprocessing as mp
+import atsp, sys, time, multiprocessing as mp
 
 #################
-cores = 16  # How many threads you want to use?
+cores = 12  # how many threads you want to use?
+epoch = 50  # at least 1, more epoch takes more time but reaches closer to the best result
 regb = (0.3, 3)  # regularization_bound
-initf = 16  # initial fitness for multiprocessing
+initf = 20  # initial fitness for multiprocessing, more fitness takes more time but reaches closer to the best result
+initc = None  # initial solution in an array format
 #################
 
+def addajob(bestres, sil_mod=True):
+    global processes, epoch, initf
 
-def atsp_array(array, init_sol):
-    _atsp = atsp.SA(list(map(int, array)), regularization_bound=regb, initial_fitness=initf, initial_solution=init_sol, silent_mode=True)
-    output.put(_atsp.solve())
+    def tsplib(init_sol):
+        _atsp = atsp.SA(data, regularization_bound=regb, initial_fitness=initf, initial_solution=init_sol, silent_mode=sil_mod)
+        output.put(_atsp.solve())
 
+    if epoch > 0:
+        _job = mp.Process(target=tsplib, args=(bestres,))
+        processes.append(_job)
+        _job.start()
+        epoch -= 1
+        initf += 1
 
 if len(sys.argv) == 2:
     with open(sys.argv[1], 'r') as fp:
-        content = fp.read().split()
+        data = list(map(int, fp.read().split()))
         output = mp.Manager().Queue()
-        res = ([], float('inf'))
-        print('Initializing:')
-        init = atsp.SA(list(map(int, content)), regularization_bound=regb).solve()
-        print('Calculating:')
-        _processes = []
-        for _ in range(cores):
-            _processes.append(mp.Process(target=atsp_array, args=(content, init[0],)))
-        for _p in _processes:
-            _p.start()
-        for _p in _processes:
-            _p.join()
-        for _ in _processes:
-            candidate = output.get()
-            if candidate[1] and candidate[1] < res[1]:
-                res = candidate
+        res, processes = ([], float('inf')), []
+        addajob(initc, False)
+        time.sleep(1)
 
-        print(res[1])
+        while processes:
+            _job = processes.pop(0)
+            _job.join()
+            _candidate = output.get()
+            if _candidate[1] and _candidate[1] < res[1]:
+                res = _candidate
+            if epoch:
+                for _ in range(cores - len(processes)):
+                    addajob(res[0])
+
+        print(' OPTIMIZED COST:', res[1])
         print(' '.join(map(str, res[0])))
